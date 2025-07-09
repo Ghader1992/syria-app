7afafe96398b5b334694ce20918e416deff88aa4
ghader <info@tydeey.com>
2025-07-08 17:09:47 -0700

    initial upload of full eo-syria project

---
+++ cirilla/android/app/src/main/kotlin/io/rnlab/cirilla/MainActivity.kt
@@ -0,0 +1,5 @@
+package sy.eoclickandgo.sy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ cirilla/lib/constants/app.dart
@@ -0,0 +1,13 @@
+const String baseUrl = 'https://eoclickandgo.sy/';
+
+const String consumerKey = 'ck_01397296388871038005265999ce48183f4e51a2';
+
+const String consumerSecret = 'cs_f0de8ae02451af8586a371158690e68949676e25';
+
+const String restPrefix = 'wp-json';
+
+const String defaultLanguage = 'ar';
+
+const List<String> languageSupport = ['ar'];
+
+const String googleClientId = '295269595518-e7s01ueadskq7sbg2k4g4dfnefpmd7vt.apps.googleusercontent.com';
